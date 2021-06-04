7706670c676530a9ff7eed59b0a96d8820a2c300
Tay Weida <weida.tay99@gmail.com>
2021-06-04 12:04:37 +0800

    Modularized all the components on homepage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router/index.js";
 //import firebase from "firebase";
 import './assets/css/styles.css';
+import VueSmoothScroll from 'vue2-smooth-scroll';
 
 /*
 var firebaseConfig = {
@@ -20,6 +21,11 @@ firebase.initializeApp(firebaseConfig);
 
 
 Vue.config.productionTip = false;
+Vue.use(VueSmoothScroll, {
+  offset: -90,
+  duration: 400,
+  updateHistory: false,
+});
 
 new Vue({
   router,
