509d4f0ab47c3f0d9dca5f6e43cc682536d859e7
Tay Weida <weida.tay99@gmail.com>
2021-07-20 16:50:40 +0800

    updated currentDate()

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -170,7 +170,7 @@ export default {
     },
     currentDate() { //do i need date?
       const current = new Date();
-      const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+      const date = `${current.getFullYear()}/${current.getMonth()+1}/${current.getDate()}`;
       return date;
     },
     emptyCart() {
