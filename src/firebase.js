1fa7e35c4935b270123bb7f3fdeb8f70695c0d32
Tay Weida <weida.tay99@gmail.com>
2021-07-31 16:57:46 +0800

    updated firebase.js for api key

--- src/firebase.js
+++ src/firebase.js
@@ -8,17 +8,6 @@ require('dotenv').config();
 
 console.log(process.env);
 
-// let config = {
-//   apiKey: "AIzaSyD9Y1A5dwPx1BFBveee1NoRuCzQRTJFpGw",
-//   authDomain: "foodleh-da2e9.firebaseapp.com",
-//   databaseURL:
-//     "https://foodleh-da2e9-default-rtdb.asia-southeast1.firebasedatabase.app",
-//   projectId: "foodleh-da2e9",
-//   storageBucket: "foodleh-da2e9.appspot.com",
-//   messagingSenderId: "698154543422",
-//   appId: "1:698154543422:web:133dce1f44f4945aeb646f",
-// };
-
 console.log(process.env.VUE_APP_API_KEY);
 console.log(process.env.VUE_APP_AUTH_DOMAIN);
 console.log(process.env.VUE_APP_DATABASE_URL);
